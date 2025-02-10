46b2f53f99b72a7e97c025e48330c101dce491c0
Pranav7901 <pranavmore96k@gmail.com>
2025-02-10 21:46:08 +0530

    Fixed Clerk middleware issue

--- middleware.js
+++ middleware.js
@@ -1,15 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
-export default function middleware(req) {
-  try {
-    return clerkMiddleware()(req);
-  } catch (error) {
-    console.error("🚨 Clerk Middleware Error:", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
-  }
-}
+export default clerkMiddleware();
 
 export const config = {
   matcher: "/((?!_next|.*\\..*|api/.*).*)",
+  runtime: "edge",
 };
