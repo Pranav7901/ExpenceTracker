efbaca6ec38e17eb3035ead867d14244d97d26e6
Pranav7901 <pranavmore96k@gmail.com>
2025-02-10 22:11:34 +0530

    Switched to experimental-edge runtime

--- middleware.js
+++ middleware.js
@@ -1,7 +1,9 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
+export const runtime = "experimental-edge";
+
 export default clerkMiddleware();
 
 export const config = {
-  matcher: "/((?!_next|.*\\..*|api/.*).*)",
+  matcher: "/((?!_next|.*\\..*|api/.*).*)", // Excludes _next, static files, and API routes
 };
